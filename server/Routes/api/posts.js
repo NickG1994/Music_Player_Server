d3cc3c9ed310c1e96ecce942c9e57424dc17f0b2
Nick <d.gaona1994@gmail.com>
2020-11-02 11:34:33 -0600

    added and initialized vue

---
+++ server/Routes/api/posts.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//Get Posts 
+router.get('/', async (req, res) => {
+    const posts = await LoadPostCollection();
+    res.send(await posts.find({}).toArray());
+})
+//Add Posts
+router.post('/', async (req, res) =>{
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+})
+
+//Delete Posts
+router.delete('/:id', async (req, res) => {
+    const posts = await LoadPostCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+async function LoadPostCollection() {
+    const client = await mongodb.MongoClient.connect(
+        'mongodb+srv://dgaona2:Fire19941220!@vueexpress.igruc.mongodb.net/vueExpress?retryWrites=true&w=majorityy', 
+        {
+        useNewUrlParser: true
+    })
+
+    return client.db('vueExpress').collection('posts');
+}
+
+module.exports = router;
