b960bf196bf46e5c5d041b3f876171cf69cb9c63
Nick <d.gaona1994@gmail.com>
2020-11-19 16:42:28 -0600

    Added header

--- server/Routes/api/music.js
+++ server/Routes/api/music.js
@@ -8,22 +8,6 @@ router.get('/', async (req, res) => {
     const posts = await LoadPostCollection();
     res.send(await posts.find({}).toArray());
 })
-//Add Posts
-router.post('/', async (req, res) =>{
-    await posts.insertOne({
-        text: req.body.text,
-        createdAt: new Date()
-    });
-    res.status(201).send();
-})
-
-//Delete Posts
-router.delete('/:id', async (req, res) => {
-    const posts = await LoadPostCollection();
-    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
-    res.status(200).send();
-});
-
 async function LoadPostCollection() {
     const client = await mongodb.MongoClient.connect(
         'mongodb+srv://dgaona2:Fire19941220!@vueexpress.igruc.mongodb.net/music?retryWrites=true&w=majority', 
